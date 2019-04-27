9f421b6f12cae483b85d0e9dc11e863c2c86990d
AntonyDamico <antonydamico29@gmail.com>
2019-04-27 16:07:32 -0400

    changing to promises

--- backend/controllers/ListController.js
+++ backend/controllers/ListController.js
@@ -3,18 +3,17 @@ import ListService from '../services/ListService';
 class ListController {
 
     static showList(req, res) {
-        if (!this.isValidRequest(req))
+        if (!ListController.isValidReq(req))
             res.status(404).send({message: 'fix the url'});
 
         const mediaType = req.params.media;
 
-        ListService.getAll(mediaType, 1, dbRes => {
-            res.status(202).send({data: dbRes});
-        })
+        ListService.getAll(mediaType, 1)
+            .then(data => res.status(202).send({data}));
     }
 
 
-    static isValidRequest(req) {
+    static isValidReq(req) {
         return ['movie', 'show'].includes(req.params.media)
     }
 }
--- backend/services/ListService.js
+++ backend/services/ListService.js
@@ -1,13 +1,16 @@
 class ListService {
 
-    static getAll(media, userId, callback) {
+    static getAll(media, userId) {
         const sql = `select * from movie 
                         inner join movie_watch_list on movie_watch_list.movie_id = movie.id 
                         where user_id = 1 `;
-        connection.query(sql, (err, res) => {
-            if (err) return err;
-            callback(res);
-        })
+
+        return new Promise((resolve, reject) => {
+            connection.query(sql, (err, res) => {
+                if (err) return reject(err);
+                resolve(res)
+            })
+        });
     }
 }
 
