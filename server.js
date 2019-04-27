b87e58e07dbe5a9ead0b36f92fd142458f922c3f
AntonyDamico <antonydamico29@gmail.com>
2019-04-26 20:57:35 -0400

    initial backend setup

---
+++ server.js
@@ -0,0 +1,7 @@
+import app from './backend/index';
+
+
+app.listen(3000, () =>{
+    console.log('listening in port 3000')
+});
+
---
+++ start.js
@@ -0,0 +1,7 @@
+// Use '@babel/preset-env'  to transpile to ES6.
+require("@babel/register")({
+    presets: ["@babel/preset-env"]
+});
+
+// Import the rest of the app.
+module.exports = require('./server.js');
