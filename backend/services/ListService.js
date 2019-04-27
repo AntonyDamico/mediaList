0a2afea54e5da9a115aa2ef97410f65e4dece858
AntonyDamico <antonydamico29@gmail.com>
2019-04-27 17:40:54 -0400

    promisifying mysql library

--- backend/services/ListService.js
+++ backend/services/ListService.js
@@ -4,13 +4,8 @@ class ListService {
         const sql = `select * from movie 
                         inner join movie_watch_list on movie_watch_list.movie_id = movie.id 
                         where user_id = 1 `;
-
-        return new Promise((resolve, reject) => {
-            connection.query(sql, (err, res) => {
-                if (err) return reject(err);
-                resolve(res)
-            })
-        });
+        return connection.queryAsync(sql)
+            .catch(err => console.log(err.sqlMessage));
     }
 }
 
