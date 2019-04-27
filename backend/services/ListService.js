9280f1e07a8b7c904cc10dc86a04932d10db75c4
AntonyDamico <antonydamico29@gmail.com>
2019-04-27 19:07:16 -0400

    implementing query builder

--- backend/services/ListService.js
+++ backend/services/ListService.js
@@ -1,41 +1,53 @@
 class ListService {
 
     static getAll(media, userId) {
-        const names = ListService.buildQueryNames(media);
-        const sql = `select movie_id, title, language, poster
-                        vote_average, release_date from ${names.fullMediaName} 
-                        inner join ${names.listTableName} on 
-                        ${names.listTableName}.${media}_id = ${names.fullMediaName}.id 
-                        where user_id = ${userId}`;
-
+        const sql = ListQueryBuilder.buildSelectQuery(media, userId);
         return connection.queryAsync(sql)
             .catch(err => console.log(err.sqlMessage));
-
     }
 
     static insert(media, userId, mediaId) {
-        const names = ListService.buildQueryNames(media);
-        const sql = `insert into ${names.listTableName} 
-                      (${media}_id, user_id) values (${mediaId}, ${userId});`;
-
+        const sql = ListQueryBuilder.buildUpdateQuery(media, userId, mediaId);
         return connection.queryAsync(sql)
             .catch(err => console.log(err.sqlMessage));
     }
 
     static delete(media, userId, mediaId) {
-        const names = ListService.buildQueryNames(media);
-        const sql = `delete from ${names.listTableName} 
-                     where user_id = ${userId} and ${media}_id = ${mediaId}`
-
+        const sql = ListQueryBuilder.buildDeleteQuery(media, userId, mediaId);
         return connection.queryAsync(sql)
             .catch(err => console.log(err.sqlMessage));
     }
 
+}
 
-    static buildQueryNames(media) {
-        let fullMediaName = media === 'show' ? 'tv_' + media : media;
-        let listTableName = media + '_watch_list';
-        return {fullMediaName, listTableName};
+
+class ListQueryBuilder {
+
+    static buildSelectQuery(media, userId) {
+        const fullMediaName = media === 'show' ? 'tv_' + media : media;
+        const tableName = ListQueryBuilder.buildTableName(media);
+        return `select movie_id, title, language, poster
+                        vote_average, release_date from ${fullMediaName} 
+                        inner join ${tableName} on 
+                        ${tableName}.${media}_id = ${fullMediaName}.id 
+                        where user_id = ${userId}`;
+    }
+
+    static buildUpdateQuery(media, userId, mediaId) {
+        const tableName = ListQueryBuilder.buildTableName(media);
+        return `insert into ${tableName} 
+                      (${media}_id, user_id) values (${mediaId}, ${userId});`;
+    }
+
+    static buildDeleteQuery(media, userId, mediaId) {
+        const tableName = ListQueryBuilder.buildTableName(media);
+        return `delete from ${tableName} 
+                     where user_id = ${userId} and ${media}_id = ${mediaId}`;
+    }
+
+
+    static buildTableName(media) {
+        return media + '_watch_list';
     }
 }
 
