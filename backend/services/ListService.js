fb583f31a579331627c74c5ac59d3872998d6a3b
AntonyDamico <antonydamico29@gmail.com>
2019-04-30 09:11:52 -0400

    knex select

--- backend/services/ListService.js
+++ backend/services/ListService.js
@@ -1,9 +1,12 @@
 class ListService {
 
     static getAll(media, userId) {
-        const sql = ListQueryBuilder.buildSelectQuery(media, userId);
-        return connection.queryAsync(sql)
-            .catch(err => console.log(err.sqlMessage));
+        const tableName = ListService.buildTableName(media);
+        const mediaName = media === 'show' ? 'tv_' + media : media;
+
+        return knex(mediaName)
+            .innerJoin(tableName, `${tableName}.${media}_id`, '=', `${mediaName}.id`)
+            .where('user_id', userId);
     }
 
     static insert(media, user_id, mediaId) {
@@ -11,42 +14,34 @@ class ListService {
             user_id,
             [media + '_id']: mediaId
         };
-        return knex(ListQueryBuilder.buildTableName(media))
+
+        return knex(ListService.buildTableName(media))
             .insert(insertObj)
-            .catch(err => console.log(err))
     }
 
     static delete(media, userId, mediaId) {
-        const sql = ListQueryBuilder.buildDeleteQuery(media, userId, mediaId);
-        return connection.queryAsync(sql)
-            .catch(err => console.log(err.sqlMessage));
+        //     const sql = ListService.buildDeleteQuery(media, userId, mediaId);
+        //     return connection.queryAsync(sql)
+        //         .catch(err => console.log(err.sqlMessage));
     }
-
-}
-
-
-class ListQueryBuilder {
-
-    static buildSelectQuery(media, userId) {
-        const fullMediaName = media === 'show' ? 'tv_' + media : media;
-        const tableName = ListQueryBuilder.buildTableName(media);
-        return `select movie_id, title, language, poster
-                        vote_average, release_date from ${fullMediaName} 
-                        inner join ${tableName} on 
-                        ${tableName}.${media}_id = ${fullMediaName}.id 
-                        where user_id = ${userId}`;
-    }
-
-    static buildDeleteQuery(media, userId, mediaId) {
-        const tableName = ListQueryBuilder.buildTableName(media);
-        return `delete from ${tableName} 
-                     where user_id = ${userId} and ${media}_id = ${mediaId}`;
-    }
-
 
     static buildTableName(media) {
         return media + '_watch_list';
     }
+
 }
+
+
+// class ListService {
+//
+//     static buildDeleteQuery(media, userId, mediaId) {
+//         const tableName = ListService.buildTableName(media);
+//         return `delete from ${tableName}
+//                      where user_id = ${userId} and ${media}_id = ${mediaId}`;
+//     }
+//
+//
+//
+// }
 
 export default ListService;
